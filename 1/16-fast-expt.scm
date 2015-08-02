4b3c5fd87db8f787fb9e34b26ee08681b09808df
Kane <zuce_zy@163.com>
2015-08-02 17:04:50 +0800

    fix wrong answer

--- 1/16-fast-expt.scm
+++ 1/16-fast-expt.scm
@@ -5,7 +5,7 @@
 
 (define (fast-expt-iter b n a)
         (cond ((= n 0) a)
-              ((even? n) (fast-expt-iter b (/ n 2) (square a)))
+              ((even? n) (fast-expt-iter (square b) (/ n 2) a))
               (else (fast-expt-iter b (- n 1) (* a b)))))
 
 (define (even? n)
