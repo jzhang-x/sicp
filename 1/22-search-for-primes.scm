91885a2f738864479099f6eeeaae242206424e46
Kane <zuce_zy@163.com>
2015-08-10 13:36:01 +0800

    1.22

--- 1/22-search-for-primes.scm
+++ 1/22-search-for-primes.scm
@@ -1,5 +1,7 @@
 ; 22-search-for-primes.scm
 
+(load "prime.scm")
+
 (define (timed-prime-test n)
   (newline)
   (display n)
@@ -12,3 +14,13 @@
 (define (report-prime elapsed-time)
   (display " *** ")
   (display elapsed-time))
+
+(define (search-for-primes n c)
+  (cond ((= c 0) (display "done!"))
+	((prime? n)
+	 (display n)
+	 (newline)
+	 (search-for-primes (+ n 1) (- c 1)))
+	(else (search-for-primes (+ n 1) c))))
+
+      
