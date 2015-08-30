4719b3cc4985a594d8283b6bda5224db1409e455
Kane <zuce_zy@163.com>
2015-08-30 20:15:20 +0800

    add 2.27 28 29

---
+++ 2/27-deep-reverse.scm
@@ -0,0 +1,10 @@
+; 27-deep-reverse.scm
+
+(define (deep-reverse tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) tree)
+        (else (reverse (list (deep-reverse (car tree)) 
+                             (deep-reverse (cadr tree)))))))
+
+(define x (list (list 1 2) (list 3 4)))
+(deep-reverse x)
---
+++ 2/29-mobile.scm
@@ -0,0 +1,45 @@
+; 29-mobile.scm
+
+(define (make-mobile left right)
+  (list left right))
+
+(define (make-branch length structure)
+  (list length structure))
+
+(define (left-branch mobile)
+  (car mobile))
+
+(define (right-branch mobile)
+  (cadr mobile))
+
+(define (branch-length branch)
+  (car branch))
+
+(define (branch-structure branch)
+  (cadr branch))
+
+(define (total-weight mobile)
+  (+ (branch-weight (left-branch mobile))
+     (branch-weight (right-branch mobile))))
+
+(define (branch-weight branch)
+  (if (pair? (branch-structure branch))
+      (total-weight (branch-structure branch))
+      (branch-structure branch)))
+
+(define (branch-torque branch)
+  (* (branch-length branch)
+     (branch-weight branch)))
+
+(define (mobile-balance? mobile)
+  (let ((left (left-branch mobile))
+        (right (right-branch mobile)))
+    (and (= (branch-torque left)
+            (branch-torque right))
+         (branch-balance? left)
+         (branch-balance? right))))
+
+(define (branch-balance? branch)
+  (if (pair? (branch-structure branch))
+      (mobile-balance? (branch-structure branch))
+      #t))
